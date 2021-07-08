5f820998bd40ed6192062e853edfd9b4225d72bf
Dhaval Parmar <dhavalkumar_m190400ca@nitc.ac.in>
2021-07-08 23:14:37 +0530

    work added

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,6 +55,19 @@ skillsHeader.forEach((el) =>{
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+  });
+
 
 /*==================== TESTIMONIAL ====================*/
 
